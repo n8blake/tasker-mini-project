c43bc251eb21f28184f16797a24a49d55fa76ac2
Nathan Blake <developer@blake-ink.com>
2021-03-01 20:50:22 -0800

    Add EVENTS array and event object to addEvent

--- assets/js/tableScript.js
+++ assets/js/tableScript.js
@@ -1,7 +1,22 @@
 const addEventBtn = document.querySelector("#addEvent");
 
-const addEvent = () => {
+var EVENTS = [];
 
+const addEvent = () => {
+	let type = $('#projectType').value;
+	let name = $('#projectName').value;
+	let rate = $('#hourlyRate').value;
+	let date = $('#datepicker').value;
+
+	let event = {};
+	event.type = type;
+	event.name = name;
+	event.rate = rate;
+	event.date = date;
+
+	EVENTS.push(event);
 }
+
+
 
 document.addEventListner(addEventBtn, addEvent);
